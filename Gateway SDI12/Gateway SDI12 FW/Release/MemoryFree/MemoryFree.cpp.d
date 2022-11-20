85f98e5b65aa8ed5545d0f8a7832fb7841543ef6
jearango00 <jearango00@gmail.com>
2022-11-20 16:57:29 -0500

    Se crea el nuevo repositorio de gateway a parte de este.

--- Gateway SDI12/Gateway SDI12 FW/Release/MemoryFree/MemoryFree.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\Jaime\AppData\Local\Temp\VMBuilds\Gateway\ SDI12\ FW\mega_atmega2560\Release\MemoryFree\MemoryFree.cpp.o: \
- C:\GITHUB\Arduino-SDI-12\Gateway\ SDI12\libraries\MemoryFree\MemoryFree.cpp \
- C:\GITHUB\Arduino-SDI-12\Gateway\ SDI12\libraries\MemoryFree\MemoryFree.h
